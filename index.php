876049a0e02b0e6723b7faf3aa2e74062aee90bb
Gian Carlo S. Victorino <155649616+giancarlo0326@users.noreply.github.com>
2024-05-07 18:47:29 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a href="index.php" class="navbar-brand">
+              <img src="https://i.postimg.cc/7hWCmcfQ/The-Swiss-Bank-5-7-2024.png" class="header-img" alt="Logo">
+            </a>  
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">               </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">               </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="balance.php">Balance</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">               </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="deposit.php">Deposit</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">               </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="withdraw.php">Withdraw</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      
+
+  <div class="container">
+    <div class="wrapper">
+        <div class="row">
+            <div id="carouselExampleFade" class="carousel slide carousel-fade">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="https://lh3.googleusercontent.com/RATqRff6LMFM0xyTnNZM1yIOrf2P4oK0g9_TjgG4n_zDRgHX3dULNabunnoUsT198TzY_x-XkbCnxk55-UTcPiVxgg4uAVFUtver7CWzKgh2VA=w1440-e365" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://dotesports.com/wp-content/uploads/2023/02/06195459/New-Project-3-2.jpg?w=1200" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://dotesports.com/wp-content/uploads/2024/01/Origin-Palkia-and-Dialga-Pokemon-Go.jpg" class="d-block w-100" alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+        </div>
+    </div>
+  </div>
+
+
+    <footer>
+      <div class="footerContainer">
+          <div class="collab">
+              <img src="https://i.postimg.cc/7hWCmcfQ/The-Swiss-Bank-5-7-2024.png" class="collab-img">
+          </div>
+      </div>
+
+      <div class="footerBottom">
+          <p>This website is for educational purposes only and no copyright infringement is intended.</p>
+          <p>Copyright &copy;2024; All images used in this website are from the Internet.</p>
+          <p>Designed by Gian Carlo S. Victorino - GIANATICS DEVS</p>
+      </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+    crossorigin="anonymous"></script>
+</body>
+</html>
